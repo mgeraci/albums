78ace7fbfb7781711a8002e2180bce573adea835
Michael P. Geraci <me@mgeraci.com>
2015-01-01 23:22:36 -0600

    add gitignore, start django app, modify some basic settings

---
+++ albums/settings.py
@@ -0,0 +1,72 @@
+"""
+Django settings for albums project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+try:
+  from local_settings import (DEBUG, SECRET_KEY, DATABASES)
+
+except Exception as e:
+  DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
+  SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+  # $DATABASE_URL must be defined for this to work
+  import dj_database_url
+  DATABASES = {}
+  DATABASES['default'] = dj_database_url.config()
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'albums.urls'
+
+WSGI_APPLICATION = 'albums.wsgi.application'
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/New_York'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
