12d9b22d3746bb51db09e5d681893501bd7f1afa
Michael P. Geraci <me@mgeraci.com>
2016-02-03 23:46:06 -0500

    and finally, fix media files in dev

--- albums/urls.py
+++ albums/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'albums.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'', include('albumapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('albumapp.urls')),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
